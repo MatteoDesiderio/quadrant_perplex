4c1b18629fb8cb1366d3213fb958e5d7b5144207
MatteoDesiderio <4matteo.desiderio@gmail.com>
2022-10-19 16:00:08 +0100

    Add unit tests

---
+++ tests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 14:12:35 2022
+
+@author: matteo
+"""
+
+import unittest
+from quadralizer import Automator
+
+
+class TestAutomator(unittest.TestCase):
+    def test_setUp(self):
+        perplex_program = "vertex"
+        inputs = ["Anna_Fugazzi"]
+        vertex = Automator(perplex_program, inputs)
+        result = vertex.perplex_program
+        self.assertEqual(result, "./vertex")
+        result = vertex.inputs
+        self.assertEqual(result, ["Anna_Fugazzi\n"])
+        
+    def test_NotFound(self):
+        perplex_program = "vertex"
+        inputs = ["Anna_Fugazzi", ""] # the second itemq is to answer "no"
+        vertex = Automator(perplex_program, inputs)
+        stdout, stderr = vertex.automate()
+        #print(stdout)
+        #print(stderr)
+        expected = "Enter a different project name (y/n)?\n"
+        self.assertEqual(stdout[-38:], expected)
+        self.assertEqual(stderr, '')
+    
+    def test_Found(self):
+        perplex_program = "vertex"
+        inputs = ["basalt"] 
+        vertex = Automator(perplex_program, inputs)
+        stdout, stderr = vertex.automate()
+        expected = "Endofjob:basalt"
+        s = stdout.replace("\n", "").replace("-", "").replace(" ", "")[-15:]
+        self.assertEqual(s, expected)
+        # print(stdout)
+        # print(stderr)
+            
+
+if __name__ == "__main__":
+    unittest.main()
